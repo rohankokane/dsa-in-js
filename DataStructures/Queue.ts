364c492d1e9ac3c9878c8d2636f6a2b9c8b57453
Rohan Kokane <rohankokane19@gmail.com>
2022-08-19 13:00:26 +0530

    queue

---
+++ DataStructures/Queue.ts
@@ -0,0 +1,41 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
+export default class Queue<T> {
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
+
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const newNode = { value: item } as Node<T>;
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = newNode;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+  deque(): T | undefined {
+    if (!this.head) return undefined;
+
+    this.length--;
+    const head = this.head;
+    this.head = this.head.next;
+
+    head.next = undefined;
+    if (this.length === 0) this.tail = undefined;
+
+    return head.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
